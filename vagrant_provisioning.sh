2e82b583ff13255ba6bb81997fb1970588f82531
Bruno Sutic <bruno@brunosutic.com>
2015-10-12 14:10:35 +0200

    Update vagrant provisioning script

--- vagrant_provisioning.sh
+++ vagrant_provisioning.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux=1.9a-1~ppa1~p
+
+# configure X11 for xclip testing
+echo "export DISPLAY='IP:0.0'" >> /home/vagrant/.bashrc
