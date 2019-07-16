226587cf7c055e0d409c5f53e9a1e101f01a881c
Niklas Baumstark <niklas.baumstark@gmail.com>
2019-07-16 22:29:40 +0200

    Fix for WSL 2 clip.exe
    Apparently clip.exe doesn't work properly with WSL 2: It doesn't exit when EOF is reached on STDIN. This is a workaround by making sure that SIGPIPE is sent on EOF.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -147,7 +147,7 @@ clipboard_copy_command() {
             echo "pbcopy"
         fi
     elif command_exists "clip.exe"; then # WSL clipboard command
-        echo "clip.exe"
+        echo "cat | clip.exe"
     elif command_exists "wl-copy"; then # wl-clipboard: Wayland clipboard utilities
         echo "wl-copy"
     elif command_exists "xsel"; then
